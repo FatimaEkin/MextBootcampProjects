ec38caf34961fbb88df47557ae1fdf170dd5e9cb
Fatima Ekin <111661799+FatimaEkin@users.noreply.github.com>
2024-06-10 21:37:48 +0300

    [Feat] NoteApp Service Added

---
+++ NotApp2/Domain/NoteService.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain;
+
+public class NoteService
+{
+   private string dosyaYolu = "/Users/fatimaekin/NotApp2/Console/notlar.txt";
+
+        public void AddNote(string note)
+        {
+            File.AppendAllText(dosyaYolu, note + "\n");
+        }
+
+        public string GetAllNotes()
+        {
+            if (File.Exists(dosyaYolu))
+            {
+                return File.ReadAllText(dosyaYolu);
+            }
+            else
+            {
+                return "Henüz kaydedilmiş bir not yok.";
+            }
+}
+}
