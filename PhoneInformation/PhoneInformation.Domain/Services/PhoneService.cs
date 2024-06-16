3213109807e16bfe853c4bd764fdeb7bab15890f
Fatima Ekin <111661799+FatimaEkin@users.noreply.github.com>
2024-06-16 22:32:19 +0300

    [Feat] GetAll Method Added

--- PhoneInformation/PhoneInformation.Domain/Services/PhoneService.cs
+++ PhoneInformation/PhoneInformation.Domain/Services/PhoneService.cs
@@ -4,15 +4,30 @@ namespace PhoneInformation.Domain;
 public class PhoneService : IPhoneService
 {   private List<Phone> _phones = new List<Phone>();
     private string _csvFilePath = "phones.csv"; 
-    
+
     public void Add(string Brand, decimal Price, string Model, Battery Battery, Screen Screen, Camera Camera)
     {
-       
+       var phone = new Phone
+            {
+                Id = Guid.NewGuid(),
+                Brand = Brand,
+                Price = Price,
+                Model = Model,
+                Battery = Battery,
+                Screen = Screen,
+                Camera = Camera
+            };
+
+            _phones.Add(phone);
+
+            // Telefonu CSV dosyasına kaydet
+            SavePhoneToCsv(phone);
     }
 
     public List<Phone> GetAll()
     {
-        throw new NotImplementedException();
+        return _phones;
     }
+    
 }
 
