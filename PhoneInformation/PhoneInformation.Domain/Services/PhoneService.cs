23a5690a154572c141c78574746e4f2b532bc103
Fatima Ekin <111661799+FatimaEkin@users.noreply.github.com>
2024-06-16 22:25:57 +0300

    [Feat] CSV Added

--- PhoneInformation/PhoneInformation.Domain/Services/PhoneService.cs
+++ PhoneInformation/PhoneInformation.Domain/Services/PhoneService.cs
@@ -2,7 +2,9 @@
 namespace PhoneInformation.Domain;
 
 public class PhoneService : IPhoneService
-{
+{   private List<Phone> _phones = new List<Phone>();
+    private string _csvFilePath = "phones.csv"; 
+    
     public void Add(string Brand, decimal Price, string Model, Battery Battery, Screen Screen, Camera Camera)
     {
        
