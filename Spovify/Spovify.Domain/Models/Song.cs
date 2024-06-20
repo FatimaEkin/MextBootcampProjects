3f23106729f0aa522cc8a504f2daaa61c0ebc723
Fatima Ekin <111661799+FatimaEkin@users.noreply.github.com>
2024-06-20 21:33:28 +0300

    Update Song.cs

--- Spovify/Spovify.Domain/Models/Song.cs
+++ Spovify/Spovify.Domain/Models/Song.cs
@@ -2,7 +2,7 @@
 
 public class Song
 {
-  public Guid Id { get; set; }
+    public Guid Id { get; set; }
     public string Name { get; set; }
     public Person Artist { get; set; }
     public Person Producer { get; set; }
