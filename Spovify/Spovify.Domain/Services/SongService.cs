4cb480c83b0b1301b9e00159c9b055e203c38fa4
Fatima Ekin <111661799+FatimaEkin@users.noreply.github.com>
2024-06-20 21:49:18 +0300

    Update SongService

--- Spovify/Spovify.Domain/Services/SongService.cs
+++ Spovify/Spovify.Domain/Services/SongService.cs
@@ -2,7 +2,7 @@
 
 public class SongService
 {
-public void AddSong(Song song)
+    public void AddSong(Song song)
     {
         var data = $"{song.Id},{song.Name},{song.Artist.Id},{song.Artist.Name},{song.Artist.Surname},{song.Artist.Nick},{song.Producer.Id},{song.Producer.Name},{song.Producer.Surname},{song.Producer.Nick},{song.Lyricist.Id},{song.Lyricist.Name},{song.Lyricist.Surname},{song.Lyricist.Nick}\n";
 
@@ -11,5 +11,16 @@ public void AddSong(Song song)
         File.AppendAllText(path, data);
 
         Console.WriteLine("Song added successfully!");
+    }
+
+    public List<Song> GetSongs()
+    {
+        var songs = new List<Song>();
+
+        var path = "/Users/fatimaekin/Desktop/MextBootcampLesson/Spovify/Spovify.Domain/Songs.txt";
+
+        var data = File.ReadAllText(path);
+
+        var lines = data.Split("\n", StringSplitOptions.RemoveEmptyEntries);
     }
 }
