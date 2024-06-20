c36ae33d378d50619f7cedd2a1cbd56f2092dbc3
Fatima Ekin <111661799+FatimaEkin@users.noreply.github.com>
2024-06-20 21:40:50 +0300

    [Feat] AddSong Method and FilePath Added

---
+++ Spovify/Spovify.Domain/Services/SongService.cs
@@ -0,0 +1,15 @@
+﻿namespace Spovify.Domain;
+
+public class SongService
+{
+public void AddSong(Song song)
+    {
+        var data = $"{song.Id},{song.Name},{song.Artist.Id},{song.Artist.Name},{song.Artist.Surname},{song.Artist.Nick},{song.Producer.Id},{song.Producer.Name},{song.Producer.Surname},{song.Producer.Nick},{song.Lyricist.Id},{song.Lyricist.Name},{song.Lyricist.Surname},{song.Lyricist.Nick}\n";
+
+        var path = "/Users/fatimaekin/Desktop/MextBootcampLesson/Spovify/Spovify.Domain/Songs.txt";
+
+        File.AppendAllText(path, data);
+
+        Console.WriteLine("Song added successfully!");
+    }
+}
