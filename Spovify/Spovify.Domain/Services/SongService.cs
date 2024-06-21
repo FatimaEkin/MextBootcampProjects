2e3c9f51db110d7bcb4eb856e4eb3e866d12f3a4
Fatima Ekin <111661799+FatimaEkin@users.noreply.github.com>
2024-06-21 22:35:21 +0300

    Update SongService.cs

--- Spovify/Spovify.Domain/Services/SongService.cs
+++ Spovify/Spovify.Domain/Services/SongService.cs
@@ -22,5 +22,31 @@ public class SongService
         var data = File.ReadAllText(path);
 
         var lines = data.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+         foreach (var line in lines)
+        {
+            // if (string.IsNullOrEmpty(line))
+            // {
+            //     continue;
+            // }
+            
+            var values = line.Split(",");
+
+            var artist = new Person()
+            {
+                Id = Guid.Parse(values[2]),
+                Name = values[3],
+                Surname = values[4],
+                Nick = values[5],
+            };
+
+            var producer = new Person()
+            {
+                Id = Guid.Parse(values[6]),
+                Name = values[7],
+                Surname = values[8],
+                Nick = values[9],
+
+            };
+
     }
 }
