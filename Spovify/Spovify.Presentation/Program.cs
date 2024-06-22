fac25b9c3a9cc2b55d3fcf185efdcb0d27cdc511
Fatima Ekin <111661799+FatimaEkin@users.noreply.github.com>
2024-06-22 23:17:24 +0300

    [Feat] Spovify.Presentation Added

---
+++ Spovify/Spovify.Presentation/Program.cs
@@ -0,0 +1,11 @@
+﻿using Spovify.Domain;
+
+Console.WriteLine("Hello, World!");
+
+var artist = new Person()
+{
+    Id = Guid.NewGuid(),
+    Name = "Adele",
+    Surname = "Adkins",
+    Nick = "Adele"
+};
