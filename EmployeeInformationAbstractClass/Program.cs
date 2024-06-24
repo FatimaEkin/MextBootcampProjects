2cc141dd24762e7012bf73843ddb1b4acdce9171
Fatima Ekin <111661799+FatimaEkin@users.noreply.github.com>
2024-06-24 22:32:44 +0300

    [Feat] EmployeeInformation Added

---
+++ EmployeeInformationAbstractClass/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+public abstract class Employee
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public string GetFullName()
+    {
+        return $"{FirstName} {LastName}";
+    }
+
+    public abstract decimal CalculateMonthlySalary();
+}
+
+
+
+public class FullTimeEmployee : Employee
+{
+    public decimal MonthlySalary { get; set; }
+
+    public override decimal CalculateMonthlySalary()
+    {
+        return MonthlySalary;
+    }
+}
+
+public class PartTimeEmployee : Employee
+{
+    public decimal HourlyRate { get; set; }
+    public int HoursWorked { get; set; }
+
+    public override decimal CalculateMonthlySalary()
+    {
+        return HourlyRate * HoursWorked;
+    }
+}
+
+
+public class Freelancer : Employee
+{
+    public decimal ProjectRate { get; set; }
+    public int ProjectsCompleted { get; set; }
+
+    public override decimal CalculateMonthlySalary()
+    {
+        return ProjectRate * ProjectsCompleted;
+    }
+}
