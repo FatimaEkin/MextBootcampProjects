7897c63cbf908f5f6f44867cddc7f4bd183a63ca
Fatima Ekin <111661799+FatimaEkin@users.noreply.github.com>
2024-06-29 23:51:13 +0300

    [Feat] Instance Added

--- StudentsApp/StudentsApp.Console/Program.cs
+++ StudentsApp/StudentsApp.Console/Program.cs
@@ -10,4 +10,32 @@ Console.WriteLine("Hello, World!");
 
  
 
+// Student student = new Student()
+// {
+//     Id = Guid.NewGuid(),
+//     Name = "Gizem Ak",
+//     Grade = GradeLevel.Junior,
+// } ;
+    
+
+// Student student2 = new()
+// {
+//      Id = Guid.NewGuid(),
+//      Name = "Enes Demir",
+//      Grade = GradeLevel.Senior,
+//  };
+   
+//  Lecture lecture = new Lecture()
+//  {
+//   Id = Guid.NewGuid(),
+//   Title = "xyz",
+//   Type = LectureType.Hybrid
+//  };
+//     context.Students.Add(student);
+//     context.Students.Add(student2);
+//     context.Lectures.Add(lecture);
+//     context.SaveChanges();
+//     Console.WriteLine($"Öğrenci İsmi: {student.Name} - Öğrenci Sınıfı: {student.Grade}");
+//     Console.WriteLine($"Başlık: {lecture.Title} - Tipi: {lecture.Type}");
+
  
