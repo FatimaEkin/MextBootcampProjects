64decc114dee84c4bfc9a9f65f0ee1a2fd9addc8
Fatima Ekin <111661799+FatimaEkin@users.noreply.github.com>
2024-07-01 10:58:52 +0300

    Update Program.cs

--- HackerRankProblems/SimpleArraySum/Program.cs
+++ HackerRankProblems/SimpleArraySum/Program.cs
@@ -1,7 +1,6 @@
-﻿class Result
+﻿
+class Result
 {
-
-   
 
     public static int simpleArraySum(List<int> ar)
     {
@@ -21,17 +20,15 @@ class Solution
 {
     public static void Main(string[] args)
     {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        
 
         int arCount = Convert.ToInt32(Console.ReadLine().Trim());
 
         List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
 
         int result = Result.simpleArraySum(ar);
+        Console.WriteLine(result);
 
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
+    
     }
 }
