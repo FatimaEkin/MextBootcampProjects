13bad5a4297683b0552e5233a7f0e14a67fff1dd
Fatima Ekin <111661799+FatimaEkin@users.noreply.github.com>
2024-07-01 10:40:50 +0300

    [Feat] HackerRankProblems Added

---
+++ HackerRankProblems/SimpleArraySum/Program.cs
@@ -0,0 +1,37 @@
+﻿class Result
+{
+
+   
+
+    public static int simpleArraySum(List<int> ar)
+    {
+       int sumOfList = 0;
+       foreach (int element in ar)
+       
+       {
+           sumOfList += element;
+       }
+       
+       return sumOfList;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.simpleArraySum(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
