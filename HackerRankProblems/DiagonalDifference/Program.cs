7a24abb2189640957eef696bd2bad94528436641
Fatima Ekin <111661799+FatimaEkin@users.noreply.github.com>
2024-07-02 08:48:44 +0300

    [Feat] PlusMinus Added

---
+++ HackerRankProblems/DiagonalDifference/Program.cs
@@ -0,0 +1,47 @@
+﻿class Result
+{
+
+    
+    public static int diagonalDifference(List<List<int>> arr)
+    {
+        int SumOfLeftToRightDia = 0;
+        int SumOfRightToLeftDia = 0;
+        
+        int leftToRightDiagonalIndex = 0;
+        int rightToLeftDiagonalIndex = arr[0].Count  - 1;
+        
+        foreach (var list in arr)
+        {
+            SumOfLeftToRightDia += list[leftToRightDiagonalIndex];
+            leftToRightDiagonalIndex++;
+            
+            SumOfRightToLeftDia += list[rightToLeftDiagonalIndex];
+            rightToLeftDiagonalIndex--;
+        }
+        
+        return Math.Abs(SumOfLeftToRightDia - SumOfRightToLeftDia);
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+       
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.diagonalDifference(arr);
+
+         Console.WriteLine(result);
+    }
+}
---
+++ HackerRankProblems/PlusMinus/Program.cs
@@ -0,0 +1,47 @@
+﻿class Result
+{
+
+
+    public static void plusMinus(List<int> arr)
+    {
+         int countPositive = 0;
+        int countNegative = 0;
+        int countZero = 0;
+        int n = arr.Count;
+
+        foreach (var num in arr)
+        {
+            if (num > 0)
+            {
+                countPositive++;
+            }
+            else if (num < 0)
+            {
+                countNegative++;
+            }
+            else
+            {
+                countZero++;
+            }
+         }
+        double ratioPositive = (double)countPositive / n;
+        double ratioNegative = (double)countNegative / n;
+        double ratioZero = (double)countZero / n;
+
+        Console.WriteLine(ratioPositive.ToString("F6"));
+        Console.WriteLine(ratioNegative.ToString("F6"));
+        Console.WriteLine(ratioZero.ToString("F6"));
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
