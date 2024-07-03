ca082369246af1d04b52553ec03b0c525fdfee1b
Fatima Ekin <111661799+FatimaEkin@users.noreply.github.com>
2024-07-03 23:51:16 +0300

    [Feat]  Program.cs

--- HackerRankProblems/MiniMaxSum/Program.cs
+++ HackerRankProblems/MiniMaxSum/Program.cs
@@ -3,28 +3,28 @@
 
     public static void miniMaxSum(List<int> arr)
     {
-       //1 2 3 4 5
+        //1 2 3 4 5
         long maxSum = 0; //14
         long minSum = 0; //10
-        
+
         int minNumberIndex = arr.IndexOf(arr.Min()); //1
         int maxNumberIndex = arr.IndexOf(arr.Max()); //3
-        
+
         for (int i = 0; i < arr.Count; i++) //4
         {
             if (i == maxNumberIndex && i != minNumberIndex)
                 maxSum += arr[i];
             else if (i == minNumberIndex && i != maxNumberIndex)
                 minSum += arr[i];
-            else if(i != minNumberIndex && i != maxNumberIndex)
+            else if (i != minNumberIndex && i != maxNumberIndex)
             {
                 minSum += arr[i];
                 maxSum += arr[i];
             }
         }
-        
-       
-        
+
+        Console.WriteLine($"{minSum} {maxSum}");
+
     }
 
 }
